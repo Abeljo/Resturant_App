785427a56b25c42545f912d327521ca3a1b6b2fe
Abeljo <abeljo86@gmail.com>
2022-04-13 22:27:01 +0300

    added firestore

--- lib/Pages/authPage.dart
+++ lib/Pages/authPage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:resturant_app/Pages/authPage.dart';
+import 'package:resturant_app/Pages/menuPage.dart';
+import 'package:resturant_app/Pages/personalPage.dart';
+import 'package:resturant_app/Pages/reservationPage.dart';
+import 'package:resturant_app/Pages/staffPage.dart';
 
 class authPage extends StatefulWidget {
   const authPage({Key? key}) : super(key: key);
--- lib/Pages/staffPage.dart
+++ lib/Pages/staffPage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:resturant_app/Pages/authPage.dart';
+import 'package:resturant_app/Pages/menuPage.dart';
+import 'package:resturant_app/Pages/personalPage.dart';
+import 'package:resturant_app/Pages/reservationPage.dart';
+import 'package:resturant_app/Pages/staffPage.dart';
 
 class staffPage extends StatefulWidget {
   const staffPage({Key? key}) : super(key: key);
